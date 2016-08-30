4472fb1501c983a31046b38c7210076b000968cd
unset <wowon7975@naver.com>
2016-08-30 05:54:46 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cigo::Application.config.secret_key_base = '23fff6fa1f84b6716b5297d2c4bb1232d1411f1a523b43a027375e2461074acff31d02a7253501d9c1e72d370cc8f5f9c4c339b3ad02599cb54a6bfb8a5f23d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cigo::Application.config.session_store :cookie_store, key: '_cigo_session'
